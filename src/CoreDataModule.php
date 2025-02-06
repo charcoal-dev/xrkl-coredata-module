012236731506dfd696811dedd3071a141a7bf6f2
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-06 13:10:03 +0400

    Add CoreDataModule class to manage ORM and database tables
    Introduce the `CoreDataModule` abstract class to handle object store, countries ORM, and brute force control functionality. This includes declaring child components and registering associated database tables for streamlined integration.

---
+++ src/CoreDataModule.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace Charcoal\Framework\Modules\CoreData;
+
+use Charcoal\App\Kernel\Build\AppBuildPartial;
+use Charcoal\App\Kernel\Orm\AbstractOrmModule;
+use Charcoal\App\Kernel\Orm\CacheStoreEnum;
+use Charcoal\App\Kernel\Orm\Db\DatabaseTableRegistry;
+use Charcoal\App\Kernel\Orm\Db\DbAwareTableEnum;
+use Charcoal\Framework\Modules\CoreData\BruteForceControl\BfcController;
+use Charcoal\Framework\Modules\CoreData\BruteForceControl\BfcTable;
+use Charcoal\Framework\Modules\CoreData\Countries\CountriesOrm;
+use Charcoal\Framework\Modules\CoreData\ObjectStore\ObjectStoreController;
+use Charcoal\Framework\Modules\CoreData\ObjectStore\ObjectStoreTable;
+
+/**
+ * Class CoreDataModule
+ * @package Charcoal\Framework\Modules\CoreData
+ */
+abstract class CoreDataModule extends AbstractOrmModule
+{
+    public ObjectStoreController $objectStore;
+    public CountriesOrm $countries;
+    public BfcController $bfc;
+
+    public function __construct(
+        AppBuildPartial                    $app,
+        CacheStoreEnum                     $cacheStore = null,
+        private readonly ?DbAwareTableEnum $objectStoreTable = null,
+        private readonly ?DbAwareTableEnum $countriesTable = null,
+        private readonly ?DbAwareTableEnum $bfcTable = null,
+    )
+    {
+        parent::__construct($app, $cacheStore);
+    }
+
+    final protected function declareChildren(AppBuildPartial $app): void
+    {
+        if ($this->objectStoreTable) {
+            $this->objectStore = new ObjectStoreController($this, $this->objectStoreTable);
+        }
+
+        if ($this->countriesTable) {
+            $this->countries = new CountriesOrm($this, $this->countriesTable);
+        }
+
+        if ($this->bfcTable) {
+            $this->bfc = new BfcController($this, $this->bfcTable);
+        }
+    }
+
+    final protected function declareDatabaseTables(DatabaseTableRegistry $tables): void
+    {
+        if ($this->objectStoreTable) {
+            $tables->register(new ObjectStoreTable($this, $this->objectStoreTable));
+        }
+
+        if ($this->countriesTable) {
+            $tables->register(new ObjectStoreTable($this, $this->countriesTable));
+        }
+
+        if ($this->bfcTable) {
+            $tables->register(new BfcTable($this, $this->bfcTable));
+        }
+    }
+}
